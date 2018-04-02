15f32d2699b3d7fd06a8fa122ffd9646408386c3
Chris Goodrow <goodrow.chris4@gmail.com>
2018-04-02 13:31:16 -0500

    finished home page and working on second page

--- UniversalAdcom/Controllers/HomeController.cs
+++ UniversalAdcom/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace UniversalAdcom.Controllers
             return View();
         }
 
+        public ActionResult HowItWorks()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ UniversalAdcom/Scripts/Home.js
@@ -0,0 +1,20 @@
+﻿    $(window).scroll(function () {
+        var partner = $('#partner').offset().top,
+         partnerOuterHeight = $(window).outerHeight(),
+         windowHeight = $(window).height(),
+         windowScroll = $(this).scrollTop();
+        console.log(windowScroll);
+
+        if ($(window).width() < 480) {
+            if (windowScroll > 2200) {
+                $('.circle-block').show(2000);
+            }
+        }
+        else {
+            if (windowScroll > 1500) {
+                $('.circle-block').show(2000);
+            }
+        }
+        
+    });
+
