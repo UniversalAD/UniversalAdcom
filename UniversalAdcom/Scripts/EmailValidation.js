296c869c679ef82625a63da09c790a7889f5148f
Chris Goodrow <goodrow.chris4@gmail.com>
2018-04-11 12:32:51 -0500

    finished validation with jquery validate and removed unused jquery module

---
+++ UniversalAdcom/Scripts/EmailValidation.js
@@ -0,0 +1,45 @@
+﻿
+////Function to validate phone
+$("#emailContact").on('submit', function (e) {
+    var isValid = $("#emailContact").valid();
+    if (isValid, validatePhone('Phone')) {
+        console.log("valid");
+        return true;
+    }
+    else {
+        console.log("not valid");
+        e.preventDefault();
+        return false;
+    }
+});
+
+
+function validatePhone(txtPhone) {
+    var a = document.getElementById(txtPhone).value;
+    var filter = /(\d{3})\-?(\d{3})\-?(\d{4})/;
+    if (filter.test(a)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+//email contact form
+$(document).ready(function () {
+    $("input[name='Phone']").keyup(function () {
+        $(this).val($(this).val().replace(/(\d{3})\-?(\d{3})\-?(\d{4})/, '$1-$2-$3'));
+
+        if (validatePhone('Phone')) {
+            $('#spnPhoneStatus').html('Valid Phone');
+            $('#spnPhoneStatus').css('color', 'green');
+        }
+
+        else {
+            $('#spnPhoneStatus').html('Invalid Phone');
+            $('#spnPhoneStatus').css('color', 'red');
+        }
+    });
+});
+
