76170fe718c7a516fa71c3ef4abd44aa8159591d
Chris Goodrow <goodrow.chris4@gmail.com>
2018-04-11 11:01:28 -0500

    finished how it works, added contact form styled email to user

---
+++ UniversalAdcom/Models/FreeArtworkContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversalAdcom.Models
+{
+    public class FreeArtworkContact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Business Name")]
+        public string BusinessName { get; set; }
+        public string Phone { get; set; }
+        public string Position { get; set; }
+        [Display(Name = "Website/Facebook")]
+        public string Website { get; set; }
+    }
+}
